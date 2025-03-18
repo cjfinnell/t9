9e0597b10a54ac43ea00155b2438b9d920a9180c
Connor Finnell <connor.finnell@udacity.com>
2025-03-18 09:21:43 -0600

    feat: Initial trie walk with tests

--- t9.go
+++ t9.go
@@ -2,41 +2,49 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"io"
+	"log/slog"
 	"os"
+	"sort"
+	"strings"
 )
 
 const wordsFilePath = "./small-words.txt"
 
-var runeToNum = map[rune]int{
-	'a': 2,
-	'b': 2,
-	'c': 2,
-	'd': 3,
-	'e': 3,
-	'f': 3,
-	'g': 4,
-	'h': 4,
-	'i': 4,
-	'j': 5,
-	'k': 5,
-	'l': 5,
-	'm': 6,
-	'n': 6,
-	'o': 6,
-	'p': 7,
-	'q': 7,
-	'r': 7,
-	's': 7,
-	't': 8,
-	'u': 8,
-	'v': 8,
-	'w': 9,
-	'x': 9,
-	'y': 9,
-	'z': 9,
-}
+var (
+	ErrInvalidInput = errors.New("invalid input")
+
+	runeToNum = map[rune]int{
+		'a': 2,
+		'b': 2,
+		'c': 2,
+		'd': 3,
+		'e': 3,
+		'f': 3,
+		'g': 4,
+		'h': 4,
+		'i': 4,
+		'j': 5,
+		'k': 5,
+		'l': 5,
+		'm': 6,
+		'n': 6,
+		'o': 6,
+		'p': 7,
+		'q': 7,
+		'r': 7,
+		's': 7,
+		't': 8,
+		'u': 8,
+		'v': 8,
+		'w': 9,
+		'x': 9,
+		'y': 9,
+		'z': 9,
+	}
+)
 
 // rune is a type alias for readability, representing a single Unicode character.
 type rune byte
@@ -49,6 +57,8 @@ type TrieNode struct {
 
 // AddWord adds a word to the trie, recursively creating nodes as needed.
 func (n *TrieNode) AddWord(word, wordPart string) {
+	slog.Debug(fmt.Sprintf("adding word %s\tpart %s", word, wordPart))
+
 	if wordPart == "" {
 		n.words = append(n.words, word)
 
@@ -66,19 +76,57 @@ func (n *TrieNode) AddWord(word, wordPart string) {
 	n.children[runeNum].AddWord(word, remainder)
 }
 
+// NextSteps returns the available valid T9 inputs from the current node.
+func (n *TrieNode) NextSteps() []int {
+	var steps []int
+	for step := range n.children {
+		steps = append(steps, step)
+	}
+
+	sortedSteps := sort.IntSlice(steps)
+	sortedSteps.Sort()
+
+	return sortedSteps
+}
+
+// Walk traverses the trie one node at a time, returning the next node and the
+// words associated with it.
+// If the input is not a valid T9 input, the current node and an error is
+// returned.
+func (n *TrieNode) Walk(t9Input int) (*TrieNode, []string, error) {
+	// If invalid input, return current node and an error
+	child, ok := n.children[t9Input]
+	if !ok {
+		return n, nil, ErrInvalidInput
+	}
+
+	return child, child.words, nil
+}
+
 // NewWordTrie creates a new trie from a file containing words.
 func NewWordTrie(wordsSource io.Reader) (*TrieNode, error) {
 	root := &TrieNode{children: make(map[int]*TrieNode)}
 
+	slog.Info("loading words into trie")
+
+	count := 0
+
 	scanner := bufio.NewScanner(wordsSource)
 	for scanner.Scan() {
-		word := scanner.Text()
-		root.AddWord(word, "")
+		word := strings.TrimSpace(scanner.Text())
+		if word == "" {
+			continue
+		}
+
+		root.AddWord(word, word)
+		count++
 	}
 
 	if err := scanner.Err(); err != nil {
 		return nil, fmt.Errorf("failed to read words file: %w", err)
 	}
+
+	slog.Info(fmt.Sprintf("loaded %d words into trie", count))
 
 	return root, nil
 }
--- t9_test.go
+++ t9_test.go
@@ -1,1 +1,92 @@
 package main
+
+import (
+	"log/slog"
+	"sort"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestTrieNodeWalk(t *testing.T) {
+	slog.SetLogLoggerLevel(slog.LevelWarn)
+
+	testWordsSource := strings.NewReader(`
+		bar
+		bat
+		beam
+		bean
+		bear
+		bird
+		cat
+		cone
+		cow
+		dog
+		dung
+	`)
+
+	root, err := NewWordTrie(testWordsSource)
+	require.NoError(t, err)
+
+	type step struct {
+		t9    int
+		next  []int
+		words []string
+		err   error
+	}
+
+	for _, tc := range []struct {
+		name  string
+		steps []step
+	}{
+		{
+			name: "happy path",
+			steps: []step{
+				{t9: 2, next: []int{2, 3, 4, 6}},
+				{t9: 2, next: []int{7, 8}},
+				{t9: 7, words: []string{"bar"}},
+			},
+		},
+		{
+			name: "invalid inputs",
+			steps: []step{
+				{t9: 2, next: []int{2, 3, 4, 6}},
+				{t9: 5, next: []int{2, 3, 4, 6}, err: ErrInvalidInput},
+				{t9: 2, next: []int{7, 8}},
+				{t9: 5, next: []int{7, 8}, err: ErrInvalidInput},
+				{t9: 7, words: []string{"bar"}},
+			},
+		},
+		{
+			name: "multiple valid end words",
+			steps: []step{
+				{t9: 2, next: []int{2, 3, 4, 6}},
+				{t9: 3, next: []int{2}},
+				{t9: 2, next: []int{6, 7}},
+				{t9: 6, words: []string{"beam", "bean"}},
+			},
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			var currentNode, nextNode *TrieNode
+			var words []string
+			var err error
+
+			currentNode = root
+
+			for i, step := range tc.steps {
+				t.Logf("taking step %d", i)
+
+				nextNode, words, err = currentNode.Walk(step.t9)
+
+				assert.Equal(t, step.next, nextNode.NextSteps())
+				assert.Equal(t, sort.StringSlice(step.words), sort.StringSlice(words))
+				assert.Equal(t, step.err, err)
+
+				currentNode = nextNode
+			}
+		})
+	}
+}
